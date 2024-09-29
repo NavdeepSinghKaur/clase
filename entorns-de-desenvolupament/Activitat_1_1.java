88c7b53f860f963a85edc9c5827901295f81fea9
Navdeep Singh K <98622361+NavdeepSinghKaur@users.noreply.github.com>
2024-09-29 20:26:18 +0200

    upload of files

---
+++ entorns-de-desenvolupament/Activitat_1_1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+ 
+public class Activitat_1_1 { 
+	
+    public static void main(String[] args)
+    {       
+		
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\nEntra el nom : ");
+        String nom = scanner.nextLine();
+
+        System.out.println("\nEntra la edat : "); 
+        int edat = scanner.nextInt(); /
+
+        final int anyNaixement = 2024 - edat;
+
+		String showEdat = (edat < 18) ? ("\n l'alumne/a " + nom + " es MENOR d'edat. Va neixer el " + anyNaixement) : ("\n l'alumne/a " + nom + " es MAJOR d'edat Va neixer el " + anyNaixement);
+        scanner.close(); // tanquem la captura del teclat        
+		
+		System.out.println(showEdat);
+		
+    }
+  
+}
+
