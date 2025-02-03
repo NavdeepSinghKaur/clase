bbab17619e08cf1fa858f5b76236cd1e28a52987
Navdeep Singh <98622361+NavdeepSinghKaur@users.noreply.github.com>
2025-02-03 19:15:35 +0100

    new files added

--- programació/InitialArrayExercices/src/arrays/Exercise4Main.java
+++ programació/InitialArrayExercices/src/arrays/Exercise4Main.java
@@ -4,7 +4,60 @@ import java.util.Random;
 
 public class Exercise4Main {
     public static void main(String[] args) {
+        Random random = new Random();
 
+        int[] numbersArray = new int[20];
+
+        for (int i = 0; i < numbersArray.length; i++) {
+            numbersArray[i] = random.nextInt(0, 20);
+        }
+        int[] result = randomPrimeGenerator(numbersArray);
+
+        System.out.println("Array de nombres primers: ");
+        for (int j : result) {
+            System.out.print(j + " ");
+        }
+        System.out.println("\nArray original: ");
+        for (int i:numbersArray) {
+            System.out.print(i + " ");
+        }
     }
-    public static int[] array
+
+    public static int[] randomPrimeGenerator(int[] numbersArray) {
+        int[] primesArray = new int[numbersArray.length];
+        int[] helperArray;
+        int counter = -1;
+        for (int i:numbersArray) {
+            boolean isPrime = true;
+            if (i == 2 || i == 1) {
+                isPrime = false;
+            }
+            for (int j = 2; j < i-1; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                }
+            }
+            if (isPrime) {
+                counter++;
+                primesArray[counter] = i;
+            }
+        }
+
+        int notZeros = 0;
+        for (int i = 0; i < primesArray.length; i++) {
+            if (primesArray[i] != 0) {
+                notZeros++;
+            }
+        }
+
+        helperArray = new int[notZeros];
+
+        for (int i = 0; i < primesArray.length; i++) {
+            if (primesArray[i] != 0) {
+                helperArray[notZeros -1] = primesArray[i];
+                notZeros--;
+            }
+        }
+        return helperArray;
+    }
 }
---
+++ programació/InitialArrayExercices/src/arrays/Exercise5Main.java
@@ -0,0 +1,7 @@
+package arrays;
+
+public class Exercise5Main {
+    public static void main(String[] args) {
+
+    }
+}
