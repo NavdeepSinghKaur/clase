004fce81be36658f427068fff7609a509f660f97
Navdeep Singh <98622361+NavdeepSinghKaur@users.noreply.github.com>
2025-02-08 19:22:58 +0100

    update - one new exercise

--- programació/InitialArrayExercices/src/arrays/Exercise10Main.java
+++ programació/InitialArrayExercices/src/arrays/Exercise10Main.java
@@ -11,11 +11,12 @@ public class Exercise10Main {
         sc.nextLine();
 
         for (int i = 0; i < testCases; i++) {
-            System.out.println("Indica el tamany de l'array de pilots: ");
+            System.out.println("\nIndica el tamany de l'array de pilots: ");
             int arrayLength = sc.nextInt();
             sc.nextLine();
 
             String[] pilotsArray = new String[arrayLength];
+            int[] lostPositions = new int[arrayLength];
 
             System.out.println("Indica el nom dels pilots separats per espai, exemple -> (Hamilton Bottas Verstappen): ");
             String pilotNames = sc.nextLine();
@@ -23,8 +24,32 @@ public class Exercise10Main {
 
             System.out.println("Ara, indica les posicions que ha perdut cada pilot separades amb espai, exemple -> (2 1 0)");
             String lostPositionsString = sc.nextLine();
+            String[] lostPositionStringArray = lostPositionsString.split(" ");
 
-            
+            for (int j = 0; j < lostPositions.length; j++) {
+                lostPositions[j] = Integer.parseInt(lostPositionStringArray[j]);
+            }
+
+            System.out.println("Array canviat: ");
+            positionCalculator(lostPositions, pilotsArray);
+            for (String s : pilotsArray) {
+                System.out.print(s + " ");
+            }
+
+        }
+
+
+    }
+
+    public static void positionCalculator(int[] lostPositions, String[] pilotsArray) {
+        for (int i = 0; i < pilotsArray.length; i++) {
+            String pointer = pilotsArray[0];
+            int j = 1;
+            while (j <= lostPositions[i]) {
+                pilotsArray[j-1] = pilotsArray[j];
+                j++;
+            }
+            pilotsArray[lostPositions[i]] = pointer;
         }
     }
 }
